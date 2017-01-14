3297fd49b8d31271c600f0acad884ac9f2f96d54
StrongStoone <hskang9@gmail.com>
2017-01-15 05:14:23 +0900

    Show usage on user input failure

--- hmark-cli.py
+++ hmark-cli.py
@@ -141,9 +141,11 @@ def main():
 	ap.add_argument("-lv", "--level", required = True,
 		help = "Abstraction level[0~4]")
 	args = vars(ap.parse_args())
-	if not args["path"].isdigit() and type(int(args['level']))==int:
+	if not args["path"].isdigit() and args['level'].isdigit():
 		generate(args['path'],int(args['level']))
 	else:
+		print "USAGE: python hmark-cli.py -p [/path/to/program] -lv [Abstraction Level]"
+		print "Abstraction level: 0 (no abstraction) or 4 (full abstraction)"
 		sys.exit()
 
 
